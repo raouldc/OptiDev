7ad79b0ef7e21343f497abddec3b5a8d0a42d9a5
victor-xiv <xiv.victor@gmail.com>
2013-10-06 20:52:30 +1300

    set namespace to SVIWithImgBtn.cs

--- P02Project/SVIWithImgBtn.cs
+++ P02Project/SVIWithImgBtn.cs
@@ -4,39 +4,42 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows;
 
-public class SVIWithImgBtn
+namespace P02Project
 {
-    // ScatterViewItem
-    public ScatterViewItem _svi { get; set; }
-    // original width and height of the _svi
-    public double _sviOrgWidth { get; set; }
-    public double _sviOrgHeight { get; set; }
-
-    // image that embedded in the _svi
-    public Image _sviImg { get; set; }
-
-    // SurfaceButton that embedded in _svi
-    public SurfaceButton _sviBtn { get; set; }
-    // original content and background color of the _sviBtn
-    public String _sviBtnOrgContent { get; set; }
-    public Brush _sviBtnOrgBackground { get; set; }
-
-    public Point _orgCenter { get; set; }
-    
-    public SVIWithImgBtn()
-	{
-	}
-
-    public SVIWithImgBtn(ScatterViewItem svi, Image img, SurfaceButton btn) 
+    public class SVIWithImgBtn
     {
-        _svi = svi;
-        _sviOrgWidth = svi.Width;
-        _sviOrgHeight = svi.Height;
+        // ScatterViewItem
+        public ScatterViewItem _svi { get; set; }
+        // original width and height of the _svi
+        public double _sviOrgWidth { get; set; }
+        public double _sviOrgHeight { get; set; }
 
-        _sviImg = img;
+        // image that embedded in the _svi
+        public Image _sviImg { get; set; }
 
-        _sviBtn = btn;
-        _sviBtnOrgContent = btn.Content.ToString() ;
-        _sviBtnOrgBackground = btn.Background;
+        // SurfaceButton that embedded in _svi
+        public SurfaceButton _sviBtn { get; set; }
+        // original content and background color of the _sviBtn
+        public String _sviBtnOrgContent { get; set; }
+        public Brush _sviBtnOrgBackground { get; set; }
+
+        public Point _orgCenter { get; set; }
+
+        public SVIWithImgBtn()
+        {
+        }
+
+        public SVIWithImgBtn(ScatterViewItem svi, Image img, SurfaceButton btn)
+        {
+            _svi = svi;
+            _sviOrgWidth = svi.Width;
+            _sviOrgHeight = svi.Height;
+
+            _sviImg = img;
+
+            _sviBtn = btn;
+            _sviBtnOrgContent = btn.Content.ToString();
+            _sviBtnOrgBackground = btn.Background;
+        }
     }
 }
