7c110665965218d6179a4ece25eddc3d6b99ba97
Sara Metz <smetz@hotmail.co.nz>
2013-10-07 17:07:07 +1300

    Starting to refactor. NOT WORKING YET. Creating separate controls for each screen which will be as a stack on the window can be popped to go back

---
+++ P02Project/Screens/TitleControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P02Project
+{
+	/// <summary>
+	/// Interaction logic for TitleControl.xaml
+	/// </summary>
+	public partial class TitleControl : UserControl
+	{
+		public TitleControl()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
---
+++ P02Project/Screens/TopLevelPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P02Project.Screens
+{
+    /// <summary>
+    /// Interaction logic for TopLevelPage.xaml
+    /// </summary>
+    public partial class TopLevelPage : Screen
+    {
+        public TopLevelPage(SurfaceWindow1 parentWindow, String title) : base(parentWindow)
+		{
+			InitializeComponent();
+            Title.Content = title;
+		}
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            ParentWindow.popScreen();
+        }
+    }
+}
