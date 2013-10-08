1197be8d7909358e9354cc9c16aedc9c46e3c6d6
Raoul <raoul_dcunha@hotmail.com>
2013-10-08 23:35:50 +1300

    finished funkygrid implementation

--- P02Project/Screens/FunkyGrid.xaml.cs
+++ P02Project/Screens/FunkyGrid.xaml.cs
@@ -31,11 +31,32 @@ namespace P02Project.Screens
         public FunkyGrid(String filename)
         {
             InitializeComponent();
-            Content temp = XMLUtilities.GetContentFromFile(filename);
+            PageModel temp;
             //TODO: Set title and subtitle
-            AboutUsPolaroid.setImage(temp.ImageList[0].Value);
             String path = System.IO.Path.Combine(System.IO.Path.GetFullPath("."), "Resources/xml/About.xml");
-            PageModel temp = XMLUtilities.GetContentFromFile(path);
+            temp = XMLUtilities.GetContentFromFile(path);
+            Dictionary<String,PoloroidControl> dictOfPolatiods = new Dictionary<String,PoloroidControl>();
+
+            dictOfPolatiods.Add("About Us",AboutUsPolaroid);
+            dictOfPolatiods.Add("Our Ambassadors",OurAmbassadorsPolaroid);
+            dictOfPolatiods.Add("Our Stories",OurStoriesPolaroid);
+            dictOfPolatiods.Add("How we Help",HowWeCanHelpPolaroid);
+            dictOfPolatiods.Add("Our History",OurHistoryPolaroid);
+            var obj = temp.ImageList;
+            var colour = Util._pageColDict["About"];
+
+            foreach (var item in obj)
+            {
+                String key = item.id;
+                var polobject = dictOfPolatiods[key];
+                polobject.setImage(item.Value);
+                polobject.setCaption(item.caption);
+                polobject.setColour(colour);
+            }
+
+
+
+
            
         }
     }
