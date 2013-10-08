2a976db952eb2b6856ecf9768bcc058a051be020
victor-xiv <xiv.victor@gmail.com>
2013-10-09 09:59:39 +1300

    fixed the merge conflicts

---
+++ P02Project/Screen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace P02Project
+{
+    public class Screen : UserControl
+    {
+        private SurfaceWindow1 topLevelWindow;
+
+        public SurfaceWindow1 ParentWindow
+        {
+            get
+            {
+                return topLevelWindow;
+            }
+        }
+
+        protected Screen() {}
+
+        public Screen(SurfaceWindow1 parentWindow)
+        {
+            this.topLevelWindow = parentWindow;
+        }
+    }
+}
+
---
+++ P02Project/Util.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Surface.Presentation.Controls;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace P02Project
+{
+    class Util
+    {
+
+        public static readonly Dictionary<string, Color> _pageColDict = new Dictionary<string, Color>()
+        {
+            { "About",              (Color)ColorConverter.ConvertFromString("#ff00aedb")},
+            { "Family Support",     (Color)ColorConverter.ConvertFromString("#ffec008b")},
+            { "How Can I Help?",    (Color)ColorConverter.ConvertFromString("#fff68e1e")},
+            { "News",               (Color)ColorConverter.ConvertFromString("#ffa13e97")},
+            { "Events",             (Color)ColorConverter.ConvertFromString("#ffbcd530")},
+            { "Contact Us",         (Color)ColorConverter.ConvertFromString("#ffe411ff")},
+            { "Play Beads",         (Color)ColorConverter.ConvertFromString("#ffee3123")}
+        };
+            
+    }
+}
