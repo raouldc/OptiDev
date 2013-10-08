9cd41cc5a7f079462ba07a30e5404085be017f2d
Wasiq Kashkari <qisaw-eboy@hotmail.com>
2013-10-08 13:45:50 +1300

    Added a basic gridview

---
+++ P02Project/GridView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P02Project
+{
+    /// <summary>
+    /// Interaction logic for GridView.xaml
+    /// </summary>
+    public partial class GridView : UserControl
+    {
+        private int numberOfItems;
+        private int numberOfRows=1;
+        private int numberOfCols;
+        //set the max rows and max cols
+        private readonly int MAXROWS = 6;
+        private readonly int MAXCOLS = 2;
+
+
+        public GridView(String Xpath)
+        {
+            InitializeComponent();
+            /*first we need to bind the xml and create a grid Layout for that*/
+            //dummy data
+            numberOfItems = 7;
+            if (numberOfItems >= 4)
+            {
+                numberOfRows = 2;
+            }
+            numberOfCols = (int)Math.Ceiling((double)numberOfItems / (double)numberOfRows);
+            //create a gridView
+            Grid g = new Grid();
+            //create the column definitions
+            for (int i =0; i < numberOfCols;i++){
+                ColumnDefinition col = new ColumnDefinition();
+                g.ColumnDefinitions.Add(col);
+            }
+        }
+    }
+}
