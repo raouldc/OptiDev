dfdf07fa46f9cf6d2a42dec2b30ca53da08c2388
Wasiq Kashkari <qisaw-eboy@hotmail.com>
2013-10-17 01:25:49 +1300

    added data item for chris and made polaroid look nicer

---
+++ P02Project/DataFetcher/DataItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace P02Project.DataFetcher
+{
+    class DataItem
+    {
+        protected String title;
+        protected String link;
+        protected String description;
+        protected Image image;
+
+        public DataItem(String title, String link, String description, Image image)
+        {
+            this.title = title;
+            this.link = link;
+            this.description = description;
+            this.image = image;
+        }
+    }
+}
