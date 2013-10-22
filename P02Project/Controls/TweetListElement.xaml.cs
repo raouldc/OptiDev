0733cd61cb506e830ca47cf6706f294dbca2da7f
victor-xiv <xiv.victor@gmail.com>
2013-10-22 20:20:02 +1300

    created a TweetListElement that can hold both txt and img

---
+++ P02Project/Controls/TweetListElement.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P02Project
+{
+	/// <summary>
+	/// Interaction logic for TweetListElement.xaml
+	/// </summary>
+	public partial class TweetListElement : UserControl
+	{
+		public TweetListElement()
+		{
+			this.InitializeComponent();
+		}
+
+        public void setTweetText(String txt)
+        {
+            _tweetTxt.Text = txt;
+        }
+
+        public void setTweetImg(BitmapImage img)
+        {
+            _tweetImg.Source = img;
+        }
+
+
+        public void setBackground(Brush bg)
+        {
+            _layout.Background = bg;
+        }
+	}
+}
