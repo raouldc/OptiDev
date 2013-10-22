7cf394481d63195ddacf25317e9e5c8c5cf6d343
Wasiq Kashkari <qisaw-eboy@hotmail.com>
2013-10-22 23:47:51 +1300

    Added Very basic Ui to Quiz, need to redesign later

--- P02Project/Screens/Game/Quiz.xaml.cs
+++ P02Project/Screens/Game/Quiz.xaml.cs
@@ -22,5 +22,10 @@ namespace P02Project.Screens.Game
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
