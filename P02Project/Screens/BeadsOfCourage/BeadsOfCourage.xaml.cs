08748b404ef5f012bf01dc9084ea5fc911ce320e
Raoul D'Cunha <raoul_dcunha@hotmail.com>
2013-10-23 21:57:33 +1300

    the beads now move correctly. also added animations

--- P02Project/Screens/BeadsOfCourage/BeadsOfCourage.xaml.cs
+++ P02Project/Screens/BeadsOfCourage/BeadsOfCourage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace P02Project.Screens.BeadsOfCourage
 {
@@ -43,6 +44,11 @@ namespace P02Project.Screens.BeadsOfCourage
 
             Image sder = (Image) sender;
             sder.RenderTransform = new RotateTransform(0);
+            MoveTo(sder, 650, 100);
+            //var trans = new TranslateTransform();
+            ////trans.X = -300;
+            //trans.Y = -300;
+            //sder.RenderTransform = trans;
             //sder.Visibility = Visibility.Collapsed;
             //display info about the bead
             //foreach (Image child in threadedBeadGrid.Children)
@@ -71,6 +77,19 @@ namespace P02Project.Screens.BeadsOfCourage
                 this.text = text;
                 this.name = name;
             }
+        }
+
+        private void MoveTo(Image target, double newX, double newY)
+        {
+            Vector offset = VisualTreeHelper.GetOffset(target);
+            var top = offset.Y;
+            var left = offset.X;
+            TranslateTransform trans = new TranslateTransform();
+            target.RenderTransform = trans;
+            DoubleAnimation anim1 = new DoubleAnimation(0, newY - top, TimeSpan.FromSeconds(1));
+            DoubleAnimation anim2 = new DoubleAnimation(0, newX - left, TimeSpan.FromSeconds(1));
+            trans.BeginAnimation(TranslateTransform.YProperty, anim1);
+            trans.BeginAnimation(TranslateTransform.XProperty, anim2);
         }
     }
 }
